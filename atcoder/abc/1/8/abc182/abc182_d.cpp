da7b4f2e0a60ad96da5fe074dbbd8161eb67a6f8
tuvsheezz <tuvshee0089@gmail.com>
2021-08-19 11:42:28 +0900

    CF1560

---
+++ atcoder/abc/1/8/abc182/abc182_d.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FAST                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0);
+#define ll long long
+#define dd double
+#define ull unsigned long long
+#define str string
+#define ivec vector<ll>
+#define dvec vector<double>
+#define repa(i, s, e) for (ll i = s; i < e; i++)
+#define repd(i, s, e) for (ll i = s; i >= e; i--)
+#define repin(a, n)          \
+  for (ll i = 0; i < n; i++) \
+  cin >> a[i]
+#define vsorta(v) sort(v.begin(), v.end())
+#define vsortd(v) sort(v.begin(), v.end(), greater<>())
+#define sort_arr(v, n) sort(v, v + n)
+#define MOD2 998244353
+#define MAX_N 100100
+
+int main()
+{
+  FAST;
+  ll n, mx = 0, s = 0, x, xmax = 0, mxx = 0;
+  cin >> n;
+
+  repa(i, 0, n)
+  {
+    cin >> x;
+    mxx += x;
+    xmax = max(xmax, mxx);
+    mx = max(mx, s + xmax);
+    s += mxx;
+  }
+
+  cout << mx << '\n';
+
+  return 0;
+}
--- template_simple.rb
+++ template_simple.rb
@@ -6,13 +6,6 @@ def cyesno(x); puts x ? :Yes : :No; end
 def uyesno(x); puts x ? :YES : :NO; end
 def array_to_hash_by_count(c); Hash.new(0).tap { |x| c.each { |z| x[z] += 1 } }; end
 
-c = 0
-s, t = gli
-for i in (0..s+1)
-  for j in (0..s+1)
-    for k in (0..s+1)
-      c+=1 if i + j + k <= s && i * j * k <= t
-    end
-  end
+gets.to_i.times do
+  
 end
-puts c
