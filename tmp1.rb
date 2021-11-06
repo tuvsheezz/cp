7f670cc336a90720acb1bd1b2e830043955d9bbc
tuvsheezz <tuvshee0089@gmail.com>
2021-11-06 15:19:17 +0900

    20211106

--- tmp1.rb
+++ tmp1.rb
@@ -1,4 +1,13 @@
-n = gets.to_i
-h = gets.to_i
-w = gets.to_i
-puts (n - h + 1) * (n - w + 1)
+def gs() = gets.chomp
+
+def gi() = gets.to_i
+
+def gli() = gets.split.map(&:to_i)
+
+def cyesno(x) = x ? :Yes : :No
+
+def uyesno(x) = x ? :YES : :NO
+
+beggin, ratata, you = gli
+
+puts
