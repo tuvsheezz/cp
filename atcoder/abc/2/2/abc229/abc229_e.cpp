730252b9aa207e9fcc94bb53f240aaa120ab2976
tuvsheezz <tuvshee0089@gmail.com>
2021-11-28 00:36:02 +0900

    20211128

---
+++ atcoder/abc/2/2/abc229/abc229_e.cpp
@@ -0,0 +1,173 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <class T>
+using IS = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define PI 3.141592653589793238462643383279502884L
+#define LL long long
+#define DD double
+#define ULL unsigned long long
+#define STR string
+template <class T>
+using V = vector<T>;
+template <class T>
+using VV = V<V<T>>;
+#define MLL map<LL, LL>
+#define MSL map<STR, LL>
+#define MLB map<LL, bool>
+template <class T>
+using PQA = priority_queue<T, V<T>, greater<T>>;
+template <class T>
+using PQD = priority_queue<T, V<T>, less<T>>;
+#define IT iterator
+#define F first
+#define S second
+#define PB push_back
+#define PLL pair<LL, LL>
+#define MP make_pair
+#define rep(i, e) for (LL i = 0; i < e; i++)
+#define repa(i, s, e) for (LL i = s; i < e; i++)
+#define repd(i, s, e) for (LL i = s; i >= e; i--)
+#define repauto(x, s) for (auto x : s)
+#define rd(...) \
+	__VA_ARGS__;  \
+	read(__VA_ARGS__)
+#define rdv(value, ...) \
+	value(__VA_ARGS__);   \
+	cin >> value
+template <class T>
+auto &operator>>(istream &is, vector<T> &xs)
+{
+	for (auto &x : xs)
+		is >> x;
+	return is;
+}
+template <class T>
+auto &operator<<(ostream &os, vector<T> &xs)
+{
+	int sz = xs.size();
+	rep(i, sz) os << xs[i] << " \n"[i + 1 == sz];
+	return os;
+}
+template <class T, class Y>
+auto &operator<<(ostream &os, pair<T, Y> &xs)
+{
+	os << "{" << xs.first << ", " << xs.second << "}";
+	return os;
+}
+template <class T, class Y>
+auto &operator>>(istream &is, vector<pair<T, Y>> &xs)
+{
+	for (auto &[x1, x2] : xs)
+		is >> x1 >> x2;
+	return is;
+}
+template <class... Args>
+auto &read(Args &...args) { return (cin >> ... >> args); }
+#define vsorta(v) sort(v.begin(), v.end())
+#define vsortd(v) sort(v.begin(), v.end(), greater<>())
+#define sort_arr(v, n) sort(v, v + n)
+#define rev(v) reverse(v.begin(), v.end())
+#define pr(x) cout << x
+#define prd(x) cout << fixed << setprecision(50) << x
+#define Yes cout << "Yes\n"
+#define YES cout << "YES\n"
+#define No cout << "No\n"
+#define NO cout << "NO\n"
+#define PN cout << '\n'
+#define PS cout << ' '
+#define Ret return 0
+#define ret return
+#define INF (1LL << 60)
+#define MOD1 1000000007
+#define MOD2 998244353
+#define MAX_N 100100
+
+struct Graph
+{
+	LL vertices;
+	V<V<LL>> edges;
+	bool is_directed;
+
+	void init(LL n, bool dir = false)
+	{
+		vertices = n;
+		edges.resize(n);
+		is_directed = dir;
+	}
+
+	void add_edge(LL u, LL v)
+	{
+		edges[u].PB(v);
+		if (!is_directed)
+			edges[v].PB(u);
+	}
+};
+
+struct UnionFind
+{
+	V<LL> parent, size;
+	LL size_max = 0, count;
+
+	void init(LL n)
+	{
+		parent.resize(n, -1);
+		size.resize(n, 1);
+		count = 0;
+		size_max = 1;
+	}
+	LL root(LL x) { ret parent[x] == -1 ? x : parent[x] = root(parent[x]); }
+	bool same(LL x, LL y) { ret root(x) == root(y); }
+	void unite(LL x, LL y)
+	{
+		x = root(x);
+		y = root(y);
+		if (x == y)
+			ret;
+
+		if (size[x] < size[y])
+			swap(x, y);
+		parent[y] = x;
+		size[x] += size[y];
+		size_max = max(size[x], size_max);
+		count--;
+	}
+	void add_vertice() { count++; }
+	LL count_of_sets() { ret count; }
+	LL get_union_size(LL x) { ret size[root(x)]; }
+};
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	LL rd(n, m);
+	Graph G;
+	G.init(n);
+	UnionFind uf;
+	uf.init(n);
+	V<LL> ans(n, 0);
+	rep(i, m)
+	{
+		LL rd(u, v);
+		G.add_edge(u - 1, v - 1);
+	}
+	repd(i, n - 1, 0)
+	{
+		ans[i] = uf.count_of_sets();
+		uf.add_vertice();
+		repauto(x, G.edges[i])
+		{
+			if (x > i)
+				uf.unite(i, x);
+		}
+	}
+	repauto(x, ans)
+	{
+		pr(x);
+		PN;
+	}
+	Ret;
+}
