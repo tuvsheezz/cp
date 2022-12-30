585533226490a0f16f8c86d26c1d73ce5c962335
tuvsheezz <tuvshee0089@gmail.com>
2022-12-31 00:11:00 +0900

    Hi

--- 1.cpp
+++ 1.cpp
@@ -87,18 +87,16 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
-  STR rd(s);
-  LL ans = 0, prev_len = 2, n = s.size();
-  rep(i, n) {
-    if(prev_len == 2) {
-      ans++;
-      prev_len = 1;
-    }
-    else {
-      if(s[i] == s[i - 1]) prev_len = 2;
-      else ans++;
-    }
+  LL rd(a, b);
+  if(a > 0 && b > 0)
+    prn("Positive");
+  else if(a <= 0 && b >= 0)
+    prn("Zero");
+  else {
+    if((b - a + 1) & 1)
+      prn("Negative");
+    else 
+      prn("Positive");
   }
-  prn(ans);
   Ret;
 }
