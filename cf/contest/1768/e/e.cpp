8025f2a35bf2588fbd6be6a2754052dc51351592
tuvsheezz <tuvshee0089@gmail.com>
2023-01-06 00:11:00 +0900

    Hi

--- cf/contest/1768/e/e.cpp
+++ cf/contest/1768/e/e.cpp
@@ -79,77 +79,35 @@ auto &read(Args &...args) { return (cin >> ... >> args); }
 #define MOD1 1000000007
 #define MOD2 998244353
 #define MAX_N 100100
-
-struct Graph
-{
-  long long vertices;
-  vector<vector<pair<long long, long long>>> edges; // PLL: (destination, cost)
-  bool is_directed;
-
-  Graph(long long n, bool dir = false)
-  {
-    vertices = n;
-    edges.resize(n);
-    is_directed = dir;
-  }
-
-  void add_edge(long long u, long long v, long long cost)
-  {
-    edges[u].push_back({v, cost});
-    if (!is_directed)
-      edges[v].push_back({u, cost});
-  }
-
-  vector<long long> Dijkstra(long long s)
-  {
-    vector<long long> dist(vertices, INF);
-    dist[s] = 0;
-    priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> next_to_visit;
-    
-    next_to_visit.push({0LL, s});
-
-    while (!next_to_visit.empty())
-    {
-      pair<long long, long long> current_node = next_to_visit.top();
-      next_to_visit.pop();
-      long long v = current_node.second;
-
-      if (dist[v] < current_node.first)
-        continue;
-
-      for(auto &edge: edges[v])
-      {
-        if (dist[edge.first] > dist[v] + edge.second)
-        {
-          dist[edge.first] = dist[v] + edge.second;
-          next_to_visit.push({dist[edge.first], edge.first});
-        }
-      }
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
     }
-    return dist;
-  }
-};
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+
+void solve()
+{
+}
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
-  LL rd(n, m);
-  VV<V<LL>> dp(110, VV<LL>(110, V<LL>(110, 0)));
-  rep(i, n) {
-    LL rd(a, b, c, w);
-    dp[a][b][c] = max(dp[a][b][c], w);
-  }
-  rep(i, 101) rep(j, 101) rep(k, 101) {
-    rep(ii, 2) rep(jj, 2) rep(kk, 2) {
-      dp[i + ii][j + jj][k + kk] = max(dp[i + ii][j + jj][k + kk], dp[i][j][k]);  
-    }
-  }
-
-  rep(i, m) {
-    LL rd(a, b, c);
-    prn(dp[a][b][c]);
+  LL rd(T);
+  while (T--)
+  {
+    solve();
   }
   return 0;
 }
