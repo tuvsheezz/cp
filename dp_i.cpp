0ed95de60715db3a59ee62536eb9dd55f5d95163
tuvsheezz <tuvshee0089@gmail.com>
2023-01-10 07:11:00 +0900

    Hi

---
+++ dp_i.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define prd() cout << fixed << setprecision(50);
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  LL rd(T);
+  while (T--)
+  {
+    solve();
+  }
+  return 0;
+}
