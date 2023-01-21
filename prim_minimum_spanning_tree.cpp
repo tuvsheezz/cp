37bfd42c0af2f6b691db7b483dc629806aeec363
tuvsheezz <tuvshee0089@gmail.com>
2023-01-21 11:11:00 +0900

    Hi

---
+++ prim_minimum_spanning_tree.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <class T>
+using PQA = priority_queue<T, V<T>, greater<T>>;
+template <class T>
+using PQD = priority_queue<T, V<T>, less<T>>;
+
+int main()
+{
+
+  return 0;
+}
