7a4d94f2039fe913b8114028044dc056cb9dce3f
tuvsheezz <tuvshee0089@gmail.com>
2023-01-21 12:11:01 +0900

    Hi

--- prim_minimum_spanning_tree.cpp
+++ prim_minimum_spanning_tree.cpp
@@ -5,6 +5,23 @@ using PQA = priority_queue<T, V<T>, greater<T>>;
 template <class T>
 using PQD = priority_queue<T, V<T>, less<T>>;
 
+struct Graph
+{
+  int vertices;
+  vector<vector<pair<int, int>>> edges;
+
+  Graph(int n){
+    vertices = n;
+    edges.resize(n);
+  }
+
+  void add_edge(int u, int v, int w) {
+    edges[u].push_back({v, w});
+    edges[v].push_back({u, w});
+  }
+};
+
+
 int main()
 {
 
