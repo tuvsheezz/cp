fb1b6b7b416ecc04b5cec069a542c3b8645c3632
tuvsheezz <tuvshee0089@gmail.com>
2023-01-25 01:05:49 +0900

    Hi

---
+++ Implement/segment_tree_big.cpp
@@ -0,0 +1,153 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.141592653589793238462643383279502884L
+#define LL long long
+#define DD double
+#define ULL unsigned long long
+#define STR string
+template <class T>
+using V = vector<T>;
+template <class T>
+using VV = V<V<T>>;
+#define MLL map<LL, LL>
+#define MSL map<STR, LL>
+#define MLB map<LL, bool>
+template <class T>
+using PQA = priority_queue<T, V<T>, greater<T>>;
+template <class T>
+using PQD = priority_queue<T, V<T>, less<T>>;
+#define IT iterator
+#define F first
+#define S second
+#define PB push_back
+#define PLL pair<LL, LL>
+#define MP make_pair
+#define rep(i, e) for (LL i = 0; i < e; i++)
+#define repa(i, s, e) for (LL i = s; i < e; i++)
+#define repd(i, s, e) for (LL i = s; i >= e; i--)
+#define repauto(x, s) for (auto x : s)
+#define rd(...) \
+  __VA_ARGS__;  \
+  read(__VA_ARGS__)
+#define rdv(value, ...) \
+  value(__VA_ARGS__);   \
+  cin >> value
+template <class T>
+auto &operator>>(istream &is, vector<T> &xs)
+{
+  for (auto &x : xs)
+    is >> x;
+  return is;
+}
+template <class T>
+auto &operator<<(ostream &os, vector<T> &xs)
+{
+  int sz = xs.size();
+  rep(i, sz) os << xs[i] << " \n"[i + 1 == sz];
+  return os;
+}
+template <class T, class Y>
+auto &operator<<(ostream &os, pair<T, Y> &xs)
+{
+  os << "{" << xs.first << ", " << xs.second << "}";
+  return os;
+}
+template <class T, class Y>
+auto &operator>>(istream &is, vector<pair<T, Y>> &xs)
+{
+  for (auto &[x1, x2] : xs)
+    is >> x1 >> x2;
+  return is;
+}
+template <class... Args>
+auto &read(Args &...args) { return (cin >> ... >> args); }
+#define vsorta(v) sort(v.begin(), v.end())
+#define vsortd(v) sort(v.begin(), v.end(), greater<>())
+#define sort_arr(v, n) sort(v, v + n)
+#define rev(v) reverse(v.begin(), v.end())
+#define pr(x) cout << x
+#define prs(x) cout << x << ' '
+#define prn(x) cout << x << '\n'
+#define prd() cout << fixed << setprecision(50);
+#define Yes cout << "Yes\n"
+#define YES cout << "YES\n"
+#define No cout << "No\n"
+#define NO cout << "NO\n"
+#define PN cout << '\n'
+#define PS cout << ' '
+#define INF (1LL << 60)
+#define MOD1 1000000007
+#define MOD2 998244353
+#define MAX_N 100100
+
+
+// hemjee n 10e9 ntr uyd hereglene.
+struct SegTree {
+    int left, right;
+    int sum = 0;
+    SegTree *left_child = nullptr, *right_child = nullptr;
+
+    SegTree(int lb, int rb) {
+        left = lb;
+        right = rb;
+    }
+
+    void extend() {
+        if (!left_child && left + 1 < right) {
+            int t = (left + right) / 2;
+            left_child = new SegTree(left, t);
+            right_child = new SegTree(t, right);
+        }
+    }
+
+    void add(int k, int x) {
+        extend();
+        sum += x;
+        if (left_child) {
+            if (k < left_child->right)
+                left_child->add(k, x);
+            else
+                right_child->add(k, x);
+        }
+    }
+
+    int get_sum(int lq, int rq) {
+        if (lq <= left && right <= rq)
+            return sum;
+        if (max(left, lq) >= min(right, rq))
+            return 0;
+        extend();
+        return left_child->get_sum(lq, rq) + right_child->get_sum(lq, rq);
+    }
+};
+
+long long big_pow(long long a, long long b, long long mod)
+{
+  long long d = 1;
+  while (b > 0)
+  {
+    if (b % 2 == 1) d = d * a % mod;
+    b /= 2;
+    a = a * a % mod;
+  }
+  return d;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  LL rd(n);
+  LL ans = 0;
+  V<LL> rdv(a, n);
+  SegTree st(0, MOD1);
+  repd(i, n - 1, 0) {
+    auto x = st.get_sum(a[i], MOD1);
+    ans = (ans + (MOD2 + big_pow(2, x, MOD2) - 1)) % MOD2;
+    st.add(a[i], 1);
+  }
+  prn(ans);
+  return 0;
+}
